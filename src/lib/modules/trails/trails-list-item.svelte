b56faac7b1293ea422153bd2a615e4e974943df4
Wojtek Malarski <w.malarski@gmail.com>
2025-10-07 22:59:46 +0200

    adds removing trail

---
+++ src/lib/modules/trails/trails-list-item.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import { _ } from "svelte-i18n";
+  import { getTrailsContext } from "./trails-context.svelte";
+  import type { TrailEntry } from "./trails-storage";
+
+  interface Props {
+    trail: TrailEntry;
+  }
+
+  const { trail }: Props = $props();
+
+  const trailsContext = getTrailsContext();
+
+  const onDeleteClick = () => {
+    trailsContext.remove(trail.id);
+  };
+</script>
+
+<li>
+  {JSON.stringify(trail, null, 2)}
+  <Button onclick={onDeleteClick}>{$_("trails.delete_trail")}</Button>
+</li>
--- src/lib/modules/trails/trails-list.svelte
+++ src/lib/modules/trails/trails-list.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { getTrailsContext } from "./trails-context.svelte";
+  import TrailsListItem from "./trails-list-item.svelte";
 
   const trailsContext = getTrailsContext();
 </script>
 
 <ul>
-  {#each trailsContext.trails as trail}
-    <li>{JSON.stringify(trail, null, 2)}</li>
+  {#each trailsContext.trails as trail (trail.id)}
+    <TrailsListItem {trail} />
   {/each}
 </ul>
