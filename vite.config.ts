ce7ff95f10a0d0652ae42572697f933b2ecb7455
Wojtek Malarski <w.malarski@gmail.com>
2025-11-02 12:08:18 +0100

    packages bumped + fixes to vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
-import path from "node:path";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 import { defineConfig } from "vite";
+import zipPack from "vite-plugin-zip-pack";
 import { manifestPlugin } from "./src/build/manifest-plugin";
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    sourcemap: true,
-    target: "esnext",
-  },
-  plugins: [svelte(), tailwindcss(), manifestPlugin()],
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
+export default defineConfig(() => {
+  const isProduction = process.env.BUILD_MODE === "production";
+  return {
+    build: {
+      sourcemap: !isProduction,
+      target: "esnext",
     },
-  },
+    plugins: [
+      svelte(),
+      tailwindcss(),
+      manifestPlugin(),
+      isProduction ? zipPack({ outFileName: "svelte-trails.zip" }) : undefined,
+    ],
+    resolve: {
+      alias: {
+        $lib: path.resolve("./src/lib"),
+      },
+    },
+  };
 });
